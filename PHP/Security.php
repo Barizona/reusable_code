26d03f88b3166db25613969f4de8d0f4dfe8ef63
Bart Van der Linden <bart.vanderlinden@student.ap.be>
2015-03-24 21:48:29 +0100

    added .
    commiting

--- PHP/Security.php
+++ PHP/Security.php
@@ -2,7 +2,7 @@
 
 /* 
  * Copyright (C) 2014 Vincent.
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
+ * MA 02110-1301  USA.
+ * /
 
 include_once 'Lib/Database.php';
 
